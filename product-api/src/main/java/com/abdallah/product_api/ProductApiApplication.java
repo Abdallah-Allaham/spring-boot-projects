7937dd58b1eb5422e1e3cec06dfdeae4e8446cc1
Abdallah-Allaham <allham.abod@gmail.com>
2025-06-06 19:11:02 +0300

    spring boot projects

---
+++ product-api/src/main/java/com/abdallah/product_api/ProductApiApplication.java
@@ -0,0 +1,23 @@
+package com.abdallah.product_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApiApplication.class, args);
+	}
+
+}
+
+//@RestController
+//class MyController{
+//	@GetMapping("/")
+//	public String home(){
+//		return "Hello, Spring Boot";
+//	}
+//}
